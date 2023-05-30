5c45ffef1eea01196178359aa44109949f4b0cbb
Huoever <997595328@qq.com>
2023-05-31 00:54:12 +0800

    feat:鼠标进入方向

---
+++ JavaScript/鼠标进入方向/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const rect = container.getBoundingClientRect();
+const theta = Math.atan(rect.height, rect.width);
+container.addEventListener("mousemove", (e) => {
+  const x = e.offsetX - rect.width / 2;
+  const y = rect.height / 2 - e.offsetY;
+  const d = Math.atan2(y, x);
+  let dir;
+  if (d < theta && d > -theta) {
+    dir = "right";
+  } else if (d >= theta && d < Math.PI - theta) {
+    dir = "top";
+  } else if (d < -theta && d > theta - Math.PI) {
+    dir = "bottom";
+  } else {
+    dir = "left";
+  }
+  container.classList.add(dir);
+});
+
+container.addEventListener("mouseleave", () => {
+  container.className = "container";
+});
